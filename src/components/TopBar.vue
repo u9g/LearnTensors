a3126698600ea0d361f35e5ed2ca190f3052f3b8
U9G <git@u9g.dev>
2026-03-24 11:44:39 -0400

    Remove engineering blog

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -161,7 +161,6 @@ function toggleTheme() {
           <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
         </svg>
       </button>
-      <a href="/blog/" class="top-bar-link">Engineering Blog</a>
     </nav>
   </header>
 </template>
--- src/entry-blog.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import BlogApp from "./BlogApp.vue";
-
-createApp(BlogApp).mount("#app");
-document.getElementById("app")!.style.visibility = "visible";
